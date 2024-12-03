3a6c49b76d56bc400912e58578ec1bfcf2ee94c5
Adilfarooque <Adilfarq11@gmail.com>
2024-12-03 22:20:18 +0530

    event stream

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
-func events(w http.ResponseWriter , r *http.Request){
+const add = ":8080"
+
+func events(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/event-stream")
+	tokens := []string{"This", "is", "a", "live", "event", "stream", "test", "from", "me"}
+	for _, token := range tokens {
+		content := fmt.Sprintf("data: %s\n\n", string(token))
+		w.Write([]byte(content))
+		w.(http.Flusher).Flush()
 		
+		//Intentional wait
+		time.Sleep(1 * time.Millisecond * 470)
+	}
 }
 
 func main() {
-
+	http.HandleFunc("/events", events)
+	http.ListenAndServe(add, nil)
 }
